6cda8be6fce89b614d44f6b97accfa5e49981951
JustRex-ctrl <ilyavo99@gmail.com>
2023-07-27 13:49:11 +0300

    change localip2

--- app.js
+++ app.js
@@ -9,7 +9,7 @@ const router = require('./routes/index');
 const handleError = require('./middlewares/handleError');
 const { requestLogger, errorLogger } = require('./middlewares/logger');
 
-const { PORT = 3000, DB_ADDRESS = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
+const { PORT = 3002, DB_ADDRESS = 'mongodb://127.0.0.1:27017/bitfilmsdb' } = process.env;
 
 const app = express();
 
